09fc769f450a55a3c8dfbbecf9f925dd0b542c7d
AlexLazo <alex.lazo1403@gmail.com>
2022-06-05 11:20:41 -0600

    Agregar archivos de proyecto.

---
+++ Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,68 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+
+        public CategoryInMemoryRepository()
+        {
+            //Add some default categories
+
+            categories = new List<Category>()
+            {
+                new Category { CategoryId = 1, Name = "Sedán", Description = "Vehículos Tipo Sedán 4 Puertas"},
+                new Category { CategoryId = 2, Name = "Pick-Up", Description = "Vehículos Tipo Pick-Up 4X4 y 4X2"},
+                new Category { CategoryId = 3, Name = "Coupé", Description = "Vehículos Tipo Coupé 2 Puertas"},
+            };
+        }
+
+        public void AddCategory(Category category)
+        {
+            if (categories.Any(x => x.Name.Equals(category.Name, StringComparison.OrdinalIgnoreCase))) return;
+
+            if (categories != null && categories.Count > 0)
+            {
+                var maxId = categories.Max(x => x.CategoryId);
+                category.CategoryId = maxId + 1;
+            }
+            else
+            {
+                category.CategoryId = 1;
+            }
+
+
+            categories.Add(category);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            var categoryToUpdate = GetCategoryById(category.CategoryId);
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return categories;
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            return categories?.FirstOrDefault(x => x.CategoryId == categoryId);
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            categories?.Remove(GetCategoryById(categoryId));
+        }
+    }
+}
---
+++ USeCases/CategoriesUseCases/ViewCategoriesUseCase.cs
@@ -0,0 +1,23 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using UseCases.DataStorePluginInterfaces;
+using UseCases.UseCaseInterfaces;
+
+namespace UseCases.CategoriesUseCases
+{
+    public class ViewCategoriesUseCase : IViewCategoriesUseCase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public ViewCategoriesUseCase(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        public IEnumerable<Category> Execute()
+        {
+            return categoryRepository.GetCategories();
+        }
+    }
+}
